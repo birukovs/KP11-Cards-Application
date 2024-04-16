79d16a89220e7550ce6d18c1dc53db25ff913ff9
Semyon <s.biryukov11@gmail.com>
2024-04-16 21:10:13 +0300

    Added WelcomeActivity

---
+++ app/src/main/java/com/example/kp11cardsapplication/WelcomeActivity.kt
@@ -0,0 +1,29 @@
+package com.example.kp11cardsapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import org.checkerframework.checker.units.qual.A
+
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_welcome)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+    fun WelcomeToAuthorization(view: View?) {
+        val intent: Intent
+        intent = Intent(this@WelcomeActivity, AuthorizationActivity::class.java)
+        startActivity(intent)
+    }
+}
